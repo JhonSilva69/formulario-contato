67dfb96a633ba9f85b6251aaa28f227c1e609a9a
jonatas <jonatas.silvapereira.jt@gmail.com>
2019-01-09 20:41:52 -0200

    adição de telefone no formulario de contato

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <form method="post">
     <label for="nome">Nome</label>
     <input name="nome" id="nome">
+    <label for="telefone">Telefone</label>
+    <input name="telefone" type="tel">
     <label for="mensagem">Mensagem</label>
     <textarea name="mensagem" id="mensagem"></textarea>
     <input type="submit" value="Enviar">
 </form>
 <?php
 if($_POST) {
-    var_dump($_POST);
     $pdo = new PDO('sqlite:contatos.sqlite3');
     $sth = $pdo->prepare(
         'INSERT INTO contato ' .
-        '(nome, mensagem) ' .
-        'VALUES (?, ?)'
+        '(nome, mensagem, telefone) ' .
+        'VALUES (?, ?, ?)'
     );
-    $sth->execute([$_POST['nome'], $_POST['mensagem']]);
+    $sth->execute([$_POST['nome'], $_POST['mensagem'], $_POST['telefone']]);
+    echo "Em breve entraremos em contato!";
 }
