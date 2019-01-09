738f39075c1b2995aa15d4f0bad3aba80da0ab65
jonatas <jonatas.silvapereira.jt@gmail.com>
2019-01-09 20:29:08 -0200

    criação da tabela e insert no banco

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<form>
+<form method="post">
     <label for="nome">Nome</label>
     <input name="nome" id="nome">
-    <label for="nome">Mensagem</label>
+    <label for="mensagem">Mensagem</label>
+    <textarea name="mensagem" id="mensagem"></textarea>
+    <input type="submit" value="Enviar">
 </form>
+<?php
+if($_POST) {
+    var_dump($_POST);
+    $pdo = new PDO('sqlite:contatos.sqlite3');
+    $sth = $pdo->prepare(
+        'INSERT INTO contato ' .
+        '(nome, mensagem) ' .
+        'VALUES (?, ?)'
+    );
+    $sth->execute([$_POST['nome'], $_POST['mensagem']]);
+}
