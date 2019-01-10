c7a5a10120d06d3ae65e667606392e806277cbee
Jônatas Silva <35507613+JhonSilva69@users.noreply.github.com>
2019-01-10 20:20:08 -0200

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,7 +1,8 @@
+
 <?php
 $pdo = new PDO('sqlite:contatos.sqlite3');
 $sth = $pdo->prepare(
     'DELETE FROM contato WHERE id = ?'
 );
 $sth->execute([$_GET['id']]);
-?>Contato exclído com sucesso!
+?>Contato excluído com sucesso!
