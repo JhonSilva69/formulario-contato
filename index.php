7b54834805ccf58ef62a74b99a23bbc4346025ea
Jônatas Silva <35507613+JhonSilva69@users.noreply.github.com>
2019-01-10 20:20:48 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-
 require_once 'formularioContato.php';
-
 if($_POST) {
     $pdo = new PDO('sqlite:contatos.sqlite3');
     $sth = $pdo->prepare(
         'INSERT INTO contato ' .
-        '(nome, mensagem, telefone) ' .
+        '(nome, mensagem, telefone) '.
         'VALUES (?, ?, ?)'
     );
-    $sth->execute([$_POST['nome'], $_POST['mensagem'], $_POST['telefone']]);
+    $sth->execute([
+        $_POST['nome'],
+        $_POST['mensagem'],
+        $_POST['telefone']
+    ]);
     echo "Em breve entraremos em contato!";
 }
