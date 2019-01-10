16877d737e085598821718fb4020f5ca9c28b8d6
Jônatas Silva <35507613+JhonSilva69@users.noreply.github.com>
2019-01-10 20:18:50 -0200

    Update listagem.php

--- listagem.php
+++ listagem.php
@@ -6,28 +6,30 @@ $sth = $pdo->prepare(
 $sth->execute();
 ?>
 <table>
-    <thead>
-        <tr>
-            <th>Nome</th>
-            <th>Telefone</th>
-            <th>Mensagem</th>
-            <th>&nbsp;</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        while($row = $sth->fetch()) {
-            ?>
-            <tr>
-                <td><?php echo $row['nome']?></td>
-                <td><?php echo $row['telefone']?></td>
-                <td><?php echo $row['mensagem']?></td>
-                <td>
-                    <a href="excluir.php?id=<?php echo $row['id'];?>">X</a>
-                    <a href="editar.php?id=<?php echo $row['id'];?>">E</a>
-                </td>
-            </tr><?php   
-        }
-        ?>
-    </tbody>
+  <thead>
+    <tr>
+      <th>Nome</th>
+      <th>Telefone</th>
+      <th>Mensagem</th>
+      <tr>&nbsp;</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+  while($row = $sth->fetch()) {
+      ?>
+      <tr>
+        <td><?php echo $row['nome']?></td>
+        <td><?php echo $row['telefone']?></td>
+        <td><?php echo $row['mensagem']?></td>
+        <td>
+          <a href="excluir.php?id=<?php
+            echo $row['id'];?>">X</a>
+          <a href="editar.php?id=<?php
+            echo $row['id'];?>">E</a>
+        </td>
+      </tr><?php
+  }
+  ?>
+  </tbody>
 </table>
