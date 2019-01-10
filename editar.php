99b565c1ecdb7716c894742d2dfaa897223968fc
Jônatas Silva <35507613+JhonSilva69@users.noreply.github.com>
2019-01-10 19:28:00 -0200

    Update editar.php

--- editar.php
+++ editar.php
@@ -1,25 +1,26 @@
+
 <?php
 if (!empty($_POST)) {
     $pdo = new PDO('sqlite:contatos.sqlite3');
     $sth = $pdo->prepare(
-        'UPDATE contato ' .
-        '   SET nome = ?' .
-        '   SET telefone = ?' .
-        '   SET mensagem = ?' .
-        '   WHERE id = ?'
+        'UPDATE contato '.
+        '   SET nome = ?'.
+        '   SET telefone = ?'.
+        '   SET mensagem = ?'.
+        ' WHERE id = ?'
     );
     $sth->execute([
         $_GET['nome'],
         $_GET['telefone'],
         $_GET['mensagem'],
-        $_GET['id'],
+        $_GET['id']
     ]);
+    echo 'Dados editados com sucesso!';
 }
-
 $pdo = new PDO('sqlite:contatos.sqlite3');
 $sth = $pdo->prepare(
     'SELECT * FROM contato WHERE id = ?'
-);
+    );
 $sth->execute([$_GET['id']]);
 $row = $sth->fetch();
 require_once 'formularioContato.php';
